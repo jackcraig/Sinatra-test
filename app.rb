6b3735c2fc26851af88b134761fc2fc92f4a2ffb
Jack Craig <jackthfc@gmail.com>
2016-08-05 16:33:16 +0100

    Initial functionality

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'net/http'
+require 'json'
+require "uri"
+
+
+get '/' do
+  # @name = ['Fido', 'Pongo', 'Ugo'].sample
+	uri = URI.parse("http://api.zoopla.co.uk/api/v1/property_listings.json?area=Oxford&api_key=#{ENV["Z_KEY"]}")
+	http = Net::HTTP.new(uri.host, uri.port)
+	response = http.request(Net::HTTP::Get.new(uri.request_uri))
+	@properties = JSON.parse(response.body)
+  erb :index
+end
+
+# get '/secret' do
+# 	'This is a secret page'
+# end
+
+# get '/hello' do
+# 	@visitor = params[:name]
+#   erb :index
+# end
