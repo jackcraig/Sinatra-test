b49c6bd7acac138ce7fb3bc34adcef7ab849b7e8
Jack Craig <jackthfc@gmail.com>
2016-08-05 21:51:53 +0100

    Testing different API parameters

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 require 'net/http'
 require 'json'
-require "uri"
+require 'uri'
 
 
 get '/' do
   # @name = ['Fido', 'Pongo', 'Ugo'].sample
-	uri = URI.parse("http://api.zoopla.co.uk/api/v1/property_listings.json?area=Oxford&api_key=#{ENV["Z_KEY"]}")
+	uri = URI.parse("http://api.zoopla.co.uk/api/v1/property_listings.json?area=Golden+Lane+Estate%2C+London+EC1Y&api_key=#{ENV["Z_KEY"]}")
 	http = Net::HTTP.new(uri.host, uri.port)
 	response = http.request(Net::HTTP::Get.new(uri.request_uri))
 	@properties = JSON.parse(response.body)
